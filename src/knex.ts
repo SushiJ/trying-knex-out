c13b08f7f42e8e4ad4f8e6f46ae4a8de901f8d1f
Sushij <sushant.wurk@gmail.com>
2023-04-18 14:22:30 +0530

    bug: couldn't connect fixed

--- src/knex.ts
+++ src/knex.ts
@@ -1,8 +1,16 @@
 import Knex from "knex";
 
+const { DB_PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 const knex = Knex({
   client: "postgresql",
-  connection: process.env.DB_URL,
+  connection: {
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    password: DB_PASSWORD,
+    user: DB_USER,
+    database: DB_NAME,
+  },
 });
 
 // Make sure DB connected
